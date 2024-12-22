2275cb74d1e283c8169318a43861b94efd22e431
Luke Puchner-Hardman <lupuchard@gmail.com>
2024-12-22 09:09:02 -0600

    Fix dependency name

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "iFixFloat",
-            dependencies: [.product(name: "simd", package: "kvSIMD.swift")]),
+            dependencies: [.product(name: "kvSIMD", package: "kvSIMD.swift")]),
         .testTarget(
             name: "iFixFloatTests",
             dependencies: ["iFixFloat"]),
